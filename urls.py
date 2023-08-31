1386dd7e8b0457b6c30988a17a4497d86497c0a8
alex01 <e726983@gmail.com>
2023-08-31 15:54:27 +0300

    31.08

--- urls.py
+++ urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_tms.views import Products, ProductsDetail
-
 urlpatterns = [
-    path('', Products.as_view()),
-    path('<int:pk>/', Products.as_view()),
+    path('admin/', admin.site.urls),
+    path('', include('rest_tms.urls')),
 ]
